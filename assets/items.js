5fd38b4df2ef9dc0f7c81fefdb9106a2cf3dcdc1
nuzcraft <nuzcraft@gmail.com>
2021-01-22 13:12:39 -0600

    part 10a - added items file
    in addition to adding items, I also fixed one of the prevous issues
    I was having. It turns out the order of scripts in index.html actually
    does matter

---
+++ assets/items.js
@@ -0,0 +1,14 @@
+
+Game.ItemRepository = new Game.Repository('items', Game.Item);
+
+Game.ItemRepository.define('apple', {
+    name: 'apple',
+    character: '%',
+    foreground: 'red'
+});
+
+Game.ItemRepository.define('rock', {
+    name: 'rock',
+    character: '*',
+    foreground: 'white'
+});
