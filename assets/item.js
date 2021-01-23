53ae633b58758d97c99d3254e2de4890f18036a8
nuzcraft <nuzcraft@gmail.com>
2021-01-23 09:00:00 -0600

    part 10b - describe function for items

--- assets/item.js
+++ assets/item.js
@@ -9,3 +9,18 @@ Game.Item = function(properties) {
 
 // make items inherit all the functionality from glyphs
 Game.Item.extend(Game.Glyph);
+
+Game.Item.prototype.describe = function() {
+    return this._name;
+};
+
+Game.Item.prototype.describeA = function(capitalize) {
+    // optional parameter to capitalize the a/an
+    var prefixes = capitalize ? ['A', 'An'] : ['a', 'an'];
+    var string = this.describe();
+    var firstLetter = string.charAt(0).toLowerCase();
+    // if word starts with a vowel, use an, else use a.
+    // note that this is not perfect.
+    var prefix = 'aeiou'.indexOf(firstLetter) >= 0 ? 1 : 0;
+    return prefixes[prefix] + ' ' + string;
+};
