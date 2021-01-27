87b00cd880bdc0c433927b3942c7ff0e8ac578ae
nuzcraft <nuzcraft@gmail.com>
2021-01-27 07:53:52 -0600

    part 11 - update item to extend DynamicGlyph instead of Glyph

--- assets/item.js
+++ assets/item.js
@@ -2,25 +2,10 @@
 Game.Item = function(properties) {
     properties = properties || {};
     // call the glyph's constructor with our set of properties
-    Game.Glyph.call(this, properties);
+    Game.DynamicGlyph.call(this, properties);
     // Instantiate any properties from the passed object
     this._name = properties['name'] || '';
 };
 
 // make items inherit all the functionality from glyphs
-Game.Item.extend(Game.Glyph);
-
-Game.Item.prototype.describe = function() {
-    return this._name;
-};
-
-Game.Item.prototype.describeA = function(capitalize) {
-    // optional parameter to capitalize the a/an
-    var prefixes = capitalize ? ['A', 'An'] : ['a', 'an'];
-    var string = this.describe();
-    var firstLetter = string.charAt(0).toLowerCase();
-    // if word starts with a vowel, use an, else use a.
-    // note that this is not perfect.
-    var prefix = 'aeiou'.indexOf(firstLetter) >= 0 ? 1 : 0;
-    return prefixes[prefix] + ' ' + string;
-};
+Game.Item.extend(Game.DynamicGlyph);
