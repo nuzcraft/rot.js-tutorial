8b7be3ef80d7e7777afdd6a4b0ee7db247e3003f
nuzcraft <nuzcraft@gmail.com>
2021-02-03 21:04:26 -0600

    added gain stat screen

--- assets/screens.js
+++ assets/screens.js
@@ -511,3 +511,47 @@ Game.Screen.wearScreen = new Game.Screen.ItemListScreen({
         return true;
     }
 });
+
+Game.Screen.gainStatScreen = {
+    setup: function(entity) {
+        // must be called before rendering
+        this._entity = entity;
+        this._options = entity.getStatOptions();
+    },
+    render: function(display) {
+        var letters = 'abcdefghijklmnopqrstuvwxyz';
+        display.drawText(0, 0, 'Choose a stat to increase: ');
+
+        // iterate through each of our options
+        for (var i = 0; i < this._options.length; i++) {
+            display.drawText(0, 2 + i,
+                letters.substring(i, i + 1) + ' - ' + this._options[i][0]);
+        }
+
+        // render remaining stat points
+        display.drawText(0, 4 + this._options.length,
+            "Remaining points: " + this._entity.getStatPoints());
+    },
+    handleInput: function(inputType, inputData) {
+        if (inputType === 'keydown') {
+            // if a letter was pressed, check if ti matches to a valid option
+            if (inputData.keyCode >= ROT.KEYS.VK_A && inputData.keyCode <= ROT.KEYS.VK_Z) {
+                // check if it maps to a valid item by subtracting 'a' from the character
+                // to know what letter of the alphabet we used
+                var index = inputData.keyCode - ROT.KEYS.VK_A;
+                if (this._options[index]) {
+                    // call the stat increasing function
+                    this._options[index][1].call(this._entity);
+                    // decrease stat points
+                    this._entity.setStatPoints(this._entity.getStatPoints() - 1);
+                    // if we have no stat points left, exit the screen, else refresh
+                    if (this._entity.getStatPoints() == 0) {
+                        Game.Screen.playScreen.setSubScreen(undefined);
+                    } else {
+                        Game.refresh();
+                    }
+                }
+            }
+        }
+    }
+};
