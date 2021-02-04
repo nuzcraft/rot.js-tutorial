297364be78a0301754a30762aeaec9a9434844a9
nuzcraft <nuzcraft@gmail.com>
2021-02-03 20:49:58 -0600

    add experience gainer mixin

--- assets/entitymixins.js
+++ assets/entitymixins.js
@@ -478,3 +478,75 @@ Game.EntityMixins.Equipper = {
         }
     }
 };
+
+Game.EntityMixins.ExperienceGainer = {
+    name: 'ExperienceGainer',
+    init: function(template) {
+        this._level = template['level'] || 1;
+        this._experience = template['experience'] || 0;
+        this._statPointsPerLevel = template['statPointsPerLevel'] || 1;
+        this._statPoints = 0;
+        // determine what stats can be leveled up
+        this._statOptions = [];
+        if (this.hasMixin('Attacker')) {
+            this._statOptions.push(['Increase attack value', this.increaseAttackValue]);
+        }
+        if (this.hasMixin('Destructible')) {
+            this._statOptions.push(['Increase defense value', this.increaseDefenseValue]);
+            this._statOptions.push(['Increase max health', this.increaseMaxHp]);
+        }
+        if (this.hasMixin('Sight')) {
+            this._statOptions.push(['Increase sight range', this.increasesSightRadius]);
+        }
+    },
+    getLevel: function() {
+        return this._level;
+    },
+    getExperience: function() {
+        return this._experience;
+    },
+    getNextLevelExperience: function() {
+        return (this._level * this._level) * 10;
+    },
+    getStatPoints: function() {
+        return this._statPoints;
+    },
+    setStatPoints: function(statPoints) {
+        this._statPoints = statPoints;
+    },
+    getStatOptions: function() {
+        return this._statOptions;
+    },
+    giveExperience: function(points) {
+        var statPointGained = 0;
+        var levelsGained = 0;
+        // loop until we've allocated all points
+        while (points > 0) {
+            // check if adding points will surpass the level threshold
+            if (this._experience + points > this.getNextLevelExperience()) {
+                // Fill our experienc till the next threshold
+                var usedPoints = this.getNextLevelExperience() - this.experience;
+                points -= usedPoints;
+                this._experience += usedPoints;
+                // level up our entity
+                this._level++;
+                levelsGained++;
+                this._statPoints += this._statPointsPerLevel;
+                statPointsGained += this._statPointsPerLevel;
+            } else {
+                // simple case - just give the experience
+                this._experience += points;
+                points = 0;
+            }
+        }
+        // check if we gained at least one level
+        if (levelsGained > 0) {
+            Game.sendMessage(this, "You advance to level %d.", [this._level]);
+            // heal the entity if possible
+            if (this.hasMixin('Destructible')) {
+                this.setHp(this.getMaxHp());
+            }
+            // TODO: Actually increase stats
+        }
+    }
+}
